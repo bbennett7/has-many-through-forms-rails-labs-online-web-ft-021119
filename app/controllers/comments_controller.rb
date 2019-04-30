816800394efa7a31e828114e04455c063758f011
Bryn Bennett <bryn.bennett@gmail.com>
2019-04-30 04:35:01 +0000

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,6 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.create(comment_params)
     @comment.user= User.find_by_id(comment_params[:user_id])
-    # @comment.user.username = User.find_by_id(comment_params[:user_id]).username
 
     redirect_to @comment.post
   end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,6 @@ class PostsController < ApplicationController
   end
 
   def create
-    #binding.pry
     @post = Post.new(post_params)
     if @post.save
       redirect_to post_path(@post)
